7b906e078708d477a8ed3a91a6540de6e51da839
chessmaster4242 <chessmaster4242@2a060ecb-2c39-0410-a787-992002912dfc>
2007-09-02 21:23:07 +0000

    git-svn-id: https://svn.code.sf.net/p/oo-open/code@18 2a060ecb-2c39-0410-a787-992002912dfc

--- OblivionOnline/main.h
+++ OblivionOnline/main.h
@@ -1,6 +1,6 @@
 /*
 
-Copyright 2007 Joseph Pearson aka chessmaster42 and Julian aka masterfreek64
+Copyright 2007  Julian aka masterfreek64 and Joseph Pearson aka chessmaster42 
 
 This file is part of OblivionOnline.
 
@@ -16,6 +16,24 @@ This file is part of OblivionOnline.
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	Linking OblivionOnline statically or dynamically with other modules is making a combined work based
+	on OblivionOnline. Thus, the terms and conditions of the GNU General Public License cover 
+	the whole combination.
+
+    In addition, as a special exception, the copyright holders of  OblivionOnline give you permission to 
+	combine OblivionOnline program with free software programs or libraries that are released under
+	the GNU LGPL and with code included in the standard release of Oblivion Script Extender by Ian Patterson (OBSE)
+	under the OBSE license (or modified versions of such code, with unchanged license). You may copy and distribute such a system 
+	following the terms of the GNU GPL for  OblivionOnline and the licenses of the other code concerned,
+	provided that you include the source code of that other code when and as the GNU GPL
+	requires distribution of source code.
+
+	Note that people who make modified versions of  OblivionOnline are not obligated to grant this special exception
+	for their modified versions; it is their choice whether to do so. 
+	The GNU General Public License gives permission to release a modified version without this
+	exception; this exception also makes it possible to release a modified version which carries 
+	forward this exception.
 */
 
 #ifndef main_h
