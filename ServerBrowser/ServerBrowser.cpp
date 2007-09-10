5298fa2b7dbc16e12b71da9d42c030d6dfa32ed1
masterfreek64 <masterfreek64@2a060ecb-2c39-0410-a787-992002912dfc>
2007-09-10 12:43:33 +0000

    git-svn-id: https://svn.code.sf.net/p/oo-open/code@189 2a060ecb-2c39-0410-a787-992002912dfc

---
+++ ServerBrowser/ServerBrowser.cpp
@@ -0,0 +1,294 @@
+/*
+
+Copyright 2007  Julian aka masterfreek64 and Joseph Pearson aka chessmaster42 
+
+This file is part of OblivionOnline.
+
+    OblivionOnline is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    OblivionOnline is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// ServerBrowser.cpp : Defines the entry point for the application.
+//
+#include "stdafx.h"
+#include <windows.h>
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <commctrl.h> // Ip Address Control
+#include <stdio.h>
+
+
+
+#include "ServerBrowser.h"
+
+#define NETWORKERROR(x) MessageBoxA(NULL,x,"Networking Error",0); WSACleanup();
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;								// current instance
+TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
+TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK ServerAdd(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+INT_PTR CALLBACK ServerStatusDlg(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+INT_PTR CALLBACK ServerBrowserDlg(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+char *FetchStatus(char *ServerIP,unsigned long port);
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+ 	// TODO: Place code here.
+	MSG msg;
+	HACCEL hAccelTable;
+	WSADATA wsa;
+	// Initialize global strings
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_SERVERBROWSER, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// Perform application initialization:
+	if (!InitInstance (hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_SERVERBROWSER));
+	
+	// Main message loop:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+	if(WSAStartup(MAKEWORD(2,0),&wsa)!=0)
+	return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+//  COMMENTS:
+//
+//    This function and its usage are only necessary if you want this code
+//    to be compatible with Win32 systems prior to the 'RegisterClassEx'
+//    function that was added to Windows 95. It is important to call this function
+//    so that the application will get 'well formed' small icons associated
+//    with it.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SERVERBROWSER));
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_SERVERBROWSER);
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   HWND hWnd;
+
+   hInst = hInstance; // Store instance handle in our global variable
+
+   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      0, 0, 300, 60, NULL, NULL, hInstance, NULL);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_COMMAND	- process the application menu
+//  WM_PAINT	- Paint the main window
+//  WM_DESTROY	- post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+	case WM_COMMAND:
+		wmId    = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId)
+		{
+		case ID_GAME:
+			system("obse_loader");
+			break;
+		case ID_EDITOR:
+			system("obse_loader -editor");
+			break;
+		case ID_SERVER_ADD:
+			DialogBox(hInst,MAKEINTRESOURCE(IDD_SERVER_ADD),hWnd,ServerAdd);
+			break;
+		case ID_SERVERBROWSER:
+			DialogBox(hInst,MAKEINTRESOURCE(IDD_SERVERBROWSER),hWnd,ServerBrowserDlg);
+			break;
+		case IDM_ABOUT:
+			DialogBox(hInst,MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		// TODO: Add any drawing code here...
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+//message Handler for "Add server" dialog
+INT_PTR CALLBACK ServerAdd(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		INITCOMMONCONTROLSEX iccs;
+		iccs.dwSize = sizeof(INITCOMMONCONTROLSEX);
+		iccs.dwICC = ICC_INTERNET_CLASSES;
+		return (INT_PTR)InitCommonControlsEx(&iccs);
+		break;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK )
+		{
+			FILE *realmfile;
+			DWORD dwTempAddress;
+			SendDlgItemMessage(hDlg,IDC_SERVERIP,IPM_GETADDRESS,0,(LPARAM)&dwTempAddress);
+			realmfile = fopen("realmlist.wth","w");
+			fprintf(realmfile,"%d.%d.%d.%d",FIRST_IPADDRESS(dwTempAddress),SECOND_IPADDRESS(dwTempAddress),THIRD_IPADDRESS(dwTempAddress),FOURTH_IPADDRESS(dwTempAddress));
+			fclose(realmfile);
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		if (LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg,LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)TRUE;
+}
+// Message handler for Server Browser
+INT_PTR CALLBACK ServerBrowserDlg(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int iNumServers, iServersReceived;
+	ServerInformation **ServerArray; //Pointer to an array
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		iNumServers = 0;
+		iServersReceived = 0;
+		SendDlgItemMessage(hDlg,IDC_SERVERS,LB_ADDSTRING,0,(LPARAM)"Currently no master server available. Server Browser not yet ready");
+		SendDlgItemMessage(hDlg,IDC_SERVERS,LB_ADDSTRING,0,(LPARAM)"Use web based server browser by supertrek32. http://ooservers.freehostia.com");
+		return (INT_PTR)TRUE;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
---
+++ ServerBrowser/resource.h
@@ -0,0 +1,69 @@
+/*
+
+Copyright 2007  Julian aka masterfreek64 and Joseph Pearson aka chessmaster42 
+
+This file is part of OblivionOnline.
+
+    OblivionOnline is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    OblivionOnline is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+// Microsoft Visual C++ generated include file.
+// Used by ServerBrowser.rc
+//
+#define IDC_MYICON                      2
+#define IDREFRESH                       3
+#define IDD_SERVERBROWSER_DIALOG        102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_SERVERBROWSER               107
+#define IDI_SMALL                       108
+#define IDC_SERVERBROWSER               109
+#define IDR_MAINFRAME                   128
+#define IDD_SERVER_ADD                  129
+#define IDD_SERVERBROWSER               130
+#define IDD_DIALOG1                     131
+#define IDD_STATUS                      131
+#define IDC_SERVERIP                    1000
+#define IDC_SERVERS                     1001
+#define IDC_SERVERSTATUS                1003
+#define ID_FILE_EDITOR                  32771
+#define ID_SERVERBROWSER_INTERNET       32772
+#define ID_SERVER_MANUAL                32773
+#define ID_SERVER_STATUS                32774
+#define ID_SERVERBROWSER                32775
+#define ID_SERVER_ADD                   32776
+#define ID_STATUS                       32777
+#define IDM_SERVERBROWSER               32778
+#define IDM_SERVER_ADD                  32779
+#define IDM_STATUS                      32780
+#define ID_GAME                         32781
+#define ID_SERVERADD                    32782
+#define IDM_ABOUT1                      32783
+#define ID_EDITOR                       32784
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32785
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
