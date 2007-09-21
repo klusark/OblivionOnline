8f46cda7cdb6fdd2dde051be948a4f038469da7c
masterfreek64 <masterfreek64@2a060ecb-2c39-0410-a787-992002912dfc>
2007-09-21 16:34:28 +0000

    git-svn-id: https://svn.code.sf.net/p/oo-open/code@224 2a060ecb-2c39-0410-a787-992002912dfc

--- OblivionOnline/MobSynch.cpp
+++ OblivionOnline/MobSynch.cpp
@@ -1,9 +1,48 @@
+/*
+
+Copyright 2007  Julian aka masterfreek64, Joseph Pearson aka chessmaster42 and Joel Teichroeb aka bobjr777
+
+This file is part of OblivionOnline.
+
+    OblivionOnline is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    OblivionOnline is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	Linking OblivionOnline statically or dynamically with other modules is making a combined work based
+	on OblivionOnline. Thus, the terms and conditions of the GNU General Public License cover 
+	the whole combination.
+
+    In addition, as a special exception, the copyright holders of  OblivionOnline give you permission to 
+	combine OblivionOnline program with free software programs or libraries that are released under
+	the GNU LGPL and with code included in the standard release of Oblivion Script Extender by Ian Patterson (OBSE)
+	under the OBSE license (or modified versions of such code, with unchanged license). You may copy and distribute such a system 
+	following the terms of the GNU GPL for  OblivionOnline and the licenses of the other code concerned,
+	provided that you include the source code of that other code when and as the GNU GPL
+	requires distribution of source code.
+
+	Note that people who make modified versions of  OblivionOnline are not obligated to grant this special exception
+	for their modified versions; it is their choice whether to do so. 
+	The GNU General Public License gives permission to release a modified version without this
+	exception; this exception also makes it possible to release a modified version which carries 
+	forward this exception.
+*/
+
 //This is just to synchronize Actors. It is highly experimental and NOT completed
 #define MOBSYNCH
 #ifdef MOBSYNCH
 #include "MobSynch.h"
 std::list<MCActorBuf> MCCache;
 DWORD MobResynchTimer ; // seperate , because no seperate packet
+bool bIsMasterClient;
 bool MCbFlushCache()
 {
 	MCCache.erase(MCCache.begin(),MCCache.end()); // flushes it
@@ -43,7 +82,7 @@ bool NetSynchNPC(Actor *Actor)
 	send(ServerSocket,(char *)&pkgBuf,sizeof(OOPkgActorUpdate),0);
 	return true;
 }
-bool MCbSynchActors() //called every frame , so extremely important
+bool MCbSynchActors() //called nearly every frame , so extremely important
 {
 	DWORD tickBuf;
 	tickBuf=GetTickCount();
@@ -67,5 +106,8 @@ bool MCbSynchActors() //called every frame , so extremely important
 	}
 	MobResynchTimer = tickBuf;
 	}
+}
+NetHandleMobUpdate(char *Packet)
+{
 }
 #endif
